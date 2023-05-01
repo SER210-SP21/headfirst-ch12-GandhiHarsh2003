05ce601bc2f91424217e8c7102a21aad9193ea32
GandhiHarsh2003 <23gandhiharsh@gmail.com>
2023-05-01 11:16:40 -0400

    chater 12 done

---
+++ app/src/main/java/edu/quinnipiac/ser210/guessinggame2/GameFragment.kt
@@ -0,0 +1,58 @@
+package edu.quinnipiac.ser210.guessinggame2
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.findNavController
+import edu.quinnipiac.ser210.guessinggame2.databinding.FragmentGameBinding
+
+
+class GameFragment : Fragment() {
+    // TODO: Rename and change types of parameters
+
+    lateinit var viewModel: GameViewModel
+    private var _binding: FragmentGameBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        _binding = FragmentGameBinding.inflate(inflater, container, false)
+        val view = binding.root
+        viewModel = ViewModelProvider(this).get(GameViewModel::class.java)
+
+        viewModel.incorrectGuesses.observe(viewLifecycleOwner, Observer { newValue ->
+            binding.incorrectGuesses.text = "Incorrect guesses: $newValue"
+        })
+        viewModel.livesLeft.observe(viewLifecycleOwner, Observer { newValue ->
+            binding.lives.text = "You have $newValue lives left"
+        })
+        viewModel.secretWordDisplay.observe(viewLifecycleOwner, Observer { newValue ->
+            binding.word.text = newValue
+        })
+
+        viewModel.gameOver.observe(viewLifecycleOwner, Observer { newValue ->
+            if(newValue){
+                val action = GameFragmentDirections.actionGameFragmentToResultFragment(viewModel.wonLostMessage())
+                view.findNavController().navigate(action)
+            }
+        })
+        binding.guessButton.setOnClickListener() {
+            viewModel.makeGuess(binding.guess.text.toString().uppercase())
+            binding.guess.text = null
+        }
+
+        return view
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
---
+++ app/src/main/java/edu/quinnipiac/ser210/guessinggame2/MainActivity.kt
@@ -0,0 +1,12 @@
+package edu.quinnipiac.ser210.guessinggame2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import edu.quinnipiac.ser210.guessinggame2.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
